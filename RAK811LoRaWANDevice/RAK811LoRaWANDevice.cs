1a517adaf4d8f7f4d38ea5142b11123327bce006
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-06-26 12:29:41 +1200

    Stbbed out projects for RAK811 module client and test application.

---
+++ RAK811LoRaWANDevice/RAK811LoRaWANDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RAK811LoRaWANDevice
+{
+   public class RAK811LoRaWANDevice
+   {
+      // Browse our samples repository: https://github.com/nanoframework/samples
+      // Check our documentation online: https://docs.nanoframework.net/
+      // Join our lively Discord community: https://discord.gg/gCyBu8T
+   }
+}
