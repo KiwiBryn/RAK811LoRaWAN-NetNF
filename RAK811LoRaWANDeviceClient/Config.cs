3d4ff2b7d978b7b4555eb16ebb3b32bf887ff017
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-09-06 16:49:51 +1200

    Added seperate file for config

---
+++ RAK811LoRaWANDeviceClient/Config.cs
@@ -0,0 +1,35 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) June 2020, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+#define OTAA
+//#define ABP
+namespace devMobile.IoT.Rak811LoRaWanDeviceClient
+{
+   using System;
+
+   public class Config
+   {
+#if OTAA
+      public const string AppEui = "...";
+      public const string AppKey = "...";
+#endif
+#if ABP
+      public const string DevAddress = "...";
+      public const string NwksKey = "...";
+      public const string AppsKey = "...";
+#endif   
+   }
+}
